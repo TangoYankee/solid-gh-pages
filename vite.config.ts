e045d92868647d05dad365143ef0004336a7c190
timothy <miller.tim108@gmail.com>
2022-05-14 09:22:28 -0400

    configure to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/solid-gh-pages/',
   plugins: [solidPlugin()],
   build: {
     target: 'esnext',
